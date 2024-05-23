032b2f4681fa93b44385625ed7843e6c10e0df2b
MrZooby <brandon.zuchowski91@gmail.com>
2024-05-23 17:12:44 -0400

    added number_of_words() function

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    word_count = number_of_words(text)
+    print(word_count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def number_of_words(words):
+    split_words = words.split()
+    return len(split_words)
 
 main()
