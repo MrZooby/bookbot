e2541aa76dc2eec16e15a960634b8e87e9ad92b1
MrZooby <brandon.zuchowski91@gmail.com>
2024-05-23 20:06:54 -0400

    added sort key and printing report

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
+    print(f"--- Begin report of {book_path} ---")
     text = get_book_text(book_path)
     word_count = number_of_words(text)
     letter_count = count_letters(text)
-    print(letter_count)
+    list_dict = list(letter_count.items())
+    list_dict.sort(reverse=True, key=sort_dict)
+    print(f"{word_count} words found in the document")
+    print()
+    for key in list_dict:
+        if key[0].isalpha() == True:
+            print(f"The '{key[0]}' character was found {key[1]} times")
+    print('--- End report ---')
 
 def get_book_text(path):
     with open(path) as f:
@@ -17,10 +25,12 @@ def count_letters(text):
     totals = {}
     lowered_text = text.lower()
     letters = lowered_text.replace(' ','')
- #   letters = sorted(letters)
     for letter in letters:
         number = letters.count(letter)
         totals.update({letter:number})
     return totals
+
+def sort_dict(dict):
+    return dict[1]
 
 main()
