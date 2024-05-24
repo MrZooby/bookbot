e6fce20d99dd0e1f606e5018b21f88869c7ce4cc
MrZooby <brandon.zuchowski91@gmail.com>
2024-05-23 20:10:57 -0400

    made it easier to read

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 def main():
     book_path = "books/frankenstein.txt"
     print(f"--- Begin report of {book_path} ---")
+    
     text = get_book_text(book_path)
     word_count = number_of_words(text)
     letter_count = count_letters(text)
     list_dict = list(letter_count.items())
     list_dict.sort(reverse=True, key=sort_dict)
+
     print(f"{word_count} words found in the document")
     print()
+
     for key in list_dict:
         if key[0].isalpha() == True:
             print(f"The '{key[0]}' character was found {key[1]} times")
+
     print('--- End report ---')
 
 def get_book_text(path):
